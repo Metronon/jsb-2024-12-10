8cdf7cd62deb34e4075c02d375487414f1b0d3ab
Metronon <bumpercar07@gmail.com>
2024-12-11 12:32:19 +0900

    remain NOT NULL field when other field NULL

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -34,7 +34,7 @@ public class QuestionController {
     }
 
     @GetMapping("/create")
-    public String questionCreate() {
+    public String questionCreate(QuestionForm questionForm) {
         return "question_form";
     }
 
