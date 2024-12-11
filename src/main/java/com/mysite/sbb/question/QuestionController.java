d8dc988de75ce88d2a5942f597e72ca6910a3b27
Metronon <bumpercar07@gmail.com>
2024-12-11 12:21:57 +0900

    Validate question null

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,9 +1,14 @@
 package com.mysite.sbb.question;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -35,10 +40,16 @@ public class QuestionController {
 
     @PostMapping("/create")
     public String QuestionCreate(
-            @RequestParam(value="subject") String subject,
-            @RequestParam(value="content") String content
+            @Valid QuestionForm questionForm,
+            BindingResult bindingResult
     ) {
-        // TODO : 질문 저장 구현
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(
+                questionForm.getSubject(),
+                questionForm.getContent()
+        );
         return "redirect:/question/list";
     }
 }
---
+++ src/main/java/com/mysite/sbb/question/QuestionForm.java
@@ -0,0 +1,17 @@
+package com.mysite.sbb.question;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QuestionForm {
+    @NotEmpty(message="제목은 빈칸으로 제출할 수 없습니다.")
+    @Size(max=200)
+    private String subject;
+
+    @NotEmpty(message="본문은 빈칸으로 제출할 수 없습니다.")
+    private String content;
+}
